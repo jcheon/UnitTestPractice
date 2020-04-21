a167c2be4e4d47bf711629eed41d8303899e0250
Jonathan Cheon <jcheon@mail.csuchico.edu>
2020-04-20 22:52:26 -0700

    more palindrome unit tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,31 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_simple_palindrome_2)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("cccc");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, not_palindrome_1)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("ab");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, not_palindrome_2)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("bcd");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome_with_spaces)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("a df fd a");
+	ASSERT_TRUE(actual);
+}
