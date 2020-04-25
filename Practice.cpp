8e96766eafa1ed3d30582c0dc2affcfca66d510b
jonathan cheon <jcheon@users.noreply.github.com>
2020-04-25 12:40:43 -0700

    Update Practice.cpp

--- Practice.cpp
+++ Practice.cpp
@@ -19,7 +19,7 @@ void Practice::sortDescending(int & first, int & second, int & third)
     second = third;
     third = temp;
   }
-  if( first < third )
+  if( first < third ) // first compared to 2nd. idk but check this one
   {
     int temp = first;
     first = third;
